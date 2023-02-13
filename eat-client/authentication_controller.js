2c238682d52bd34d473f4866b2ef4a4c25e19afc
Max de Groot <maxim.de.groot@ah.nl>
2023-02-14 00:10:40 +0100

    Login page and store JWTs in cookie

--- eat-client/authentication_controller.js
+++ eat-client/authentication_controller.js
@@ -1,13 +1,22 @@
 const axios = require('axios').default;
 
 const URL = 'http://localhost:8081/users/auth';
+const requestSettings =  {
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
 
-function register(user) {       
-    const res = axios.post(URL + "/register", user, {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then((result) => {
+/**
+ * Register a user with a username and password.
+ * Also get a token after succesful registration
+ * 
+ * @param user - the user to register
+ * @returns JWT if registered successfully
+ */
+async function register(user) {       
+    await axios.post(URL + "/register", user, requestSettings)
+    .then((result) => {
         return result.data.token;
     }).catch((error) => {
         console.log(error);
@@ -15,4 +24,20 @@ function register(user) {
     });
 }
 
-module.exports = { register }
+/**
+ * Authenticate a user with a username and password.
+ * Returns a JWT if authentication was succesful.
+ * 
+ * @param user - contains username and password of user to authenticate
+ * @returns JWT if authenticated successffuly
+ */
+async function authenticate(user) {
+    try {
+        const response = await axios.post('http://localhost:8081/users/auth/authenticate', user, requestSettings);
+        return response.data.token;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+module.exports = { register, authenticate }
--- eat-client/index.js
+++ eat-client/index.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+
 const mealController = require('./meal_controller');
 const authenticationController = require('./authentication_controller')
+
 var path = require('path');
 
 const port = 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.sendFile("index.html", { root: "./public" });
 });
 
+/**
+ * Go to registration page
+ */
 app.get("/register", (req, res) => {
     res.sendFile("register.html", { root: "./public" });
+});
+
+/**
+ * Go to login page
+ */
+app.get("/login", (req, res) => {
+    res.sendFile("login.html", { root: "./public" });
 });
 
 app.post("/", (req, res) => {
@@ -23,10 +37,32 @@ app.post("/", (req, res) => {
     res.status = 200;
 })
 
+/**
+ * Create an accoutn and redirect to the correct page
+ * if registered successfully, set cookie of JWT as well
+ * 
+ * TODO: error handling + correct redirect
+ */
 app.post('/register', (req, res) => {
-    authenticationController.register(req.body);
-    res.status = 200;
-    res.sendFile("register.html", { root: "./public" });
+    authenticationController.register(req.body).then(token => res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .redirect("/")
+    );
+});
+
+/**
+ * Login to your account and redirect to the correct page
+ * if logged in successfully, set cookie of JWT as well
+ * 
+ * TODO: error handling + correct redirect
+ */
+app.post('/login', (req, res) => {
+    authenticationController.authenticate(req.body).then(token => res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .redirect("/")
+    );
 });
 
 app.listen(port, () => {
